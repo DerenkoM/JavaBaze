6c889f2882023bd86ce189fc94c2eb9072ed5a2c
Mila <derenko-m@inbox.ru>
2023-03-02 11:03:01 +0700

    Add point 2 and 3 in Numbers_Digit

--- src/ru/autotesting/base/Numbers_Digit.java
+++ src/ru/autotesting/base/Numbers_Digit.java
@@ -7,12 +7,34 @@ public class Numbers_Digit {
         System.out.println("Введите целое число:");
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
+        int b = a;
         int summa = 0;
 
-        while (a != 0) {
-            summa += (a % 10);
-            a /= 10;
+        while (b != 0) {
+            summa += (b % 10);
+            b /= 10;
         }
         System.out.println("Сумма цифр Вашего числа равна " + summa);
+
+        int max = 0;
+        b = a;
+        while (b != 0) {
+            int max1 = (b % 10);
+            if (max1 > max) {
+                max = max1;
+            }
+            b /= 10;
+        }
+        System.out.println("Максимальная цифра в Вашем числе: " + max);
+
+        b = a;
+        summa = 0;
+        while (b != 0) {
+            if ((b % 2) == 1) {
+                summa += (b % 10);
+            }
+            b /= 10;
+        }
+        System.out.println("Сумма нечетных цифр Вашего числа равна " + summa);
     }
 }
