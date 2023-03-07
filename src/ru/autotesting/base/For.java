3f10a323357bd04949206cd4ea12edacf5303537
Mila <derenko-m@inbox.ru>
2023-03-07 10:59:18 +0700

    Сorrections after the fourth remark

--- src/ru/autotesting/base/For.java
+++ src/ru/autotesting/base/For.java
@@ -3,18 +3,17 @@ package ru.autotesting.base;
 public class For {
     public static void main(String[] args) {
 
-        int RangeBegin = 1, RangeEnd = 100;
-        System.out.println("Числа кратные 4 в диапазоне от " + RangeEnd + " до " + RangeBegin + " :");
-        for (int i = RangeEnd; i >= RangeBegin; i--) {
-            if (fun(i) == true) {
+        int rangeBegin = 1, rangeEnd = 100;
+        System.out.println("Числа кратные 4 в диапазоне от " + rangeEnd + " до " + rangeBegin + " :");
+        for (int i = rangeEnd; i >= rangeBegin; i--) {
+            if (fun(i)) {
                 System.out.print(i + " / ");
             }
+
         }
     }
 
     public static boolean fun(int a) {
-        if ((a % 4) == 0) {
-            return true;
-        } else return false;
+        return a % 4 == 0;
     }
 }
